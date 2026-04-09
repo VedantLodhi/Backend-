95be2c683be968a84ba43218844279a5761e671b
VedantLodhi <116107319+VedantLodhi@users.noreply.github.com>
2026-04-09 10:27:23 +0530

    Add Register controller and bug fixes

--- public/src/controllers/user.controllers.js
+++ public/src/controllers/user.controllers.js
@@ -1,11 +1,88 @@
 import { response } from 'express';
 import {asyncHandler} from '../utils/asyncHandler.js';
+import (ApiError) from '../utils/ApiError.js';
+import {User} from '../models/User.model.js';
+import {uploadOnCloudinary} from '../utils/Cloudinary.js';55
+import {ApiResponse} from '../utils/ApiResponse.js';
 
-const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message: "Backend is working fine" 
+
+
+const registerUser = asyncHandler(async (req,res) => 
+    {
+    // STEPS TO REGISTER A USER  
+
+    // STEP-1: VALIDATE THE USER DETAILS
+    const { fullName, email, username, password } = req.body;
+    console.log("email", email);
+
+    if(fullName === "" || email === "" || username === "" || password === "") 
+    {
+        throw new ApiError(400, "All fields are required"); 
+    }    
+
+    // STEP-2: CHECK IF THE USER ALREADY EXISTS
+    const existedUser = await User.findOne({
+        $or: [{username}, {email}]
     })
-})
 
+    if(existedUser)
+    {
+        throw new ApiError(409, "User already exists with the provided email or username");
+    }
+
+    // CHECK FOR IMAGE UPLOAD & AVATAR CREATION 
+    //  HANDLE IMAGE UPLOAD USING MULTER
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // HANDLE COVER IMAGE UPLOAD
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // CHECK IF AVATAR IMAGE IS UPLOADED  
+    if(!avatarLocalPath)
+    {
+        throw new ApiError(400, "Avatar image is required");
+    }
+    
+    // STEP-4: UPLOAD THE AVATAR IMAGE & COVER IMAGE TO CLOUDINARY AND GET THE URL
+
+    // UPLOAD THE AVATAR IMAGE TO CLOUDINARY AND GET THE URL
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    // UPLOAD THE COVER IMAGE TO CLOUDINARY AND GET THE URL
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    // CHECK IF AVATAR UPLOAD WAS SUCCESSFUL
+    if(!avatar)
+    {
+        throw new ApiError(400, "Error while uploading avatar image");
+    }
+
+    // STEP-5: CREATE A NEW USER OBJECT - CREATE ENTRY IN THE DATABASE
+    const user = await User.create
+    ({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        username: username.toLowerCase(),
+        email,
+        password
+    })
+ 
+    // STEP-6: CHECK IF THE USER WAS CREATED SUCCESSFULLY
+    const createdUser = await User.findById(user._id).select(
+    // STEP-7: REMOVE PASSWORD AND REFRESH TOKEN FIELD FROM THE RESPONSE
+        "-password -refreshToken"
+    )
+
+    //  STEP-8: CHECK IF THE USER WAS CREATED SUCCESSFULLY
+    if(!createdUser)
+    {
+        throw new ApiError(500, "Error while creating user");
+    }
+
+    // STEP-9: RETURN A RESPONSE TO THE FRONTEND
+    return res.status(201).json
+    (
+        new ApiResponse(200, createdUser, "User registered successfully")
+    );
+
+})
 
 export {registerUser,}
--- public/src/routes/user.routes.js
+++ public/src/routes/user.routes.js
@@ -1,9 +1,26 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
+import {upload} from "../middlewares/multer.middlewares.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    // HANDLE IMAGE UPLOAD USING MULTER
+    upload.fields([
+        {
+            // HANDLE AVATAR UPLOAD
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            // HANDLE COVER IMAGE UPLOAD
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    // CALL THE CONTROLLER FUNCTION TO REGISTER THE USER
+    registerUser
+     );
 
 
 export default router;
