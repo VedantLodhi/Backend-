f125086015ed81041e2b713f1c6c17f3ef8fba13
VedantLodhi <116107319+VedantLodhi@users.noreply.github.com>
2026-04-10 12:34:04 +0530

    fixed auth.middlewares.js

--- public/src/middlewares/auth.middlewares.js
+++ public/src/middlewares/auth.middlewares.js
@@ -1,4 +1,3 @@
-/*
 import { ApiError } from '../utils/ApiError.js';
 import {asyncHandler} from '../utils/asyncHandler.js';
 import jwt from 'jsonwebtoken';
@@ -33,35 +32,3 @@ export const verifyJWT = asyncHandler(async (req,res,next) =>
     }
 
 })
-*/
-
-import { ApiError } from '../utils/ApiError.js';
-import { asyncHandler } from '../utils/asyncHandler.js';
-import jwt from 'jsonwebtoken';
-import { User } from '../models/user.model.js';
-
-export const verifyJWT = asyncHandler(async (req, res, next) => {
-
-    // STEP-1: EXTRACT TOKEN
-    const token =
-        req.cookies?.accessToken ||
-        req.headers?.authorization?.replace("Bearer ", "");
-
-    if (!token) {
-        throw new ApiError(401, "Unauthorized: No token provided");
-    }
-
-    // STEP-2: VERIFY TOKEN
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    // STEP-3: FIND USER
-    const user = await User.findById(decoded?._id).select("-password -refreshToken");
-
-    if (!user) {
-        throw new ApiError(401, "Unauthorized: Invalid token");
-    }
-
-    // STEP-4: ATTACH USER
-    req.user = user;
-    next();
-});
